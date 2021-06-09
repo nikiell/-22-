1338558f0ecfd4fc1558e7a875a00762e816ffd5
nikiell <72104814+nikiell@users.noreply.github.com>
2021-06-10 00:06:27 +0300

    Add files via upload

---
+++ Sem2/Lab 2 WPF/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Number_Click_2;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            combo.Items.Add(text.Text);
+            text.Text = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            combo.Items.RemoveAt(combo.Items.Count-1);
+            
+        }
+        private int count = 1;
+        private void Number_Click_2(object sender, RoutedEventArgs e)
+         {
+            string s = (string)((Button)e.OriginalSource).Content;
+            if (s == count.ToString())
+            {
+                Progress.Value += 6.25;
+                (sender as Button).Visibility = Visibility.Collapsed;
+                count++;
+            }
+            else
+            {
+                foreach (UIElement c in LayoutRoot.Children)
+                {
+                    if (c is Button)
+                    {
+                        ((Button)c).Visibility = Visibility.Visible;
+                        count = 1;
+                        Progress.Value = 0;
+                    }
+                }
+            }
+            //this.tab2.Visibility(sender as Button);
+             //this.tab2.(sender as Button).Visibility = Visibility.Visible;
+        }
+        
+    }
+}
---
+++ Sem2/Lab1WinForms/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string leftop = "";
+        string operation = "";
+        string rightop = "";
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var button in Controls.OfType<Button>())
+            {
+                button.Click += Number_Click;
+            }
+        }
+
+        private void Number_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string s = btn.Text;
+            textBox1.Text += btn.Text;
+            int num;
+            bool result = Int32.TryParse(s, out num);
+
+            if (result == true)
+            {
+
+                if (operation == "")
+                {
+
+                    leftop += s;
+                }
+                else
+                {
+
+                    rightop += s;
+                }
+            }
+
+            else
+            {
+
+                if (s == "=")
+                {
+                    Update_RightOp();
+                    textBox1.Text += rightop;
+                    operation = "";
+                }
+
+                else if (s == "CLEAR")
+                {
+                    leftop = "";
+                    rightop = "";
+                    operation = "";
+                    textBox1.Text = "";
+                }
+
+                else
+                {
+
+                    if (rightop != "")
+                    {
+                        Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+
+        private void Update_RightOp()
+        {
+            int num1 = Int32.Parse(leftop);
+            int num2 = Int32.Parse(rightop);
+
+            switch (operation)
+            {
+                case "+":
+                    rightop = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    rightop = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    rightop = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    rightop = (num1 / num2).ToString();
+                    break;
+            }
+        }
+    }
+    
+}
